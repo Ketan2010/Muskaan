0fd87eb75ab21b741cee726ff996fe202d6d4999
ketan patil <k2p1020@gmail.com>
2021-06-19 17:29:05 +0530

    commit from ketan

--- screens/Home.js
+++ screens/Home.js
@@ -3,37 +3,36 @@ import { View, Text, Button } from 'react-native'
 import firebase from '@firebase/app';
 require('firebase/auth');
 require('firebase/database');
-
+//hi darshan
 
 export default function Home({navigation}) {
     const user = firebase.auth().currentUser;
 
+
     firebase.database()
-    .ref("users/")
-    .orderByChild("uid")
-    .equalTo(user.uid)
-    .once('value')
-    .then(snapshot => {
-        if (snapshot.exists()) {
-            //user already exist no need to add in db
-        } else {
-            firebase.database().ref('users/').push({
-                uid: user.uid,
-                email: user.email,
-              
-            })
-            // try catch
-        }
+        .ref("users/")
+        .orderByChild("uid")
+        .equalTo(user.uid)
+        .once('value')
+        .then(snapshot => {
+            if (snapshot.exists()) {
+                //user already exist no need to add in db
+
+            } else {
+                firebase.database().ref('users/').push({
+                    uid: user.uid,
+                    email: user.email,              
+                })
+            }
     });
 
     const onSignoutPress = () =>{
         firebase.auth().signOut()
     }
     return (
-        <View>
-            <Text> Hello {user.email}</Text>
-            <Button title='Logout'onPress={onSignoutPress} ></Button>
-            {/* <Button title='Wheel'  onPress={()=>navigation.navigate('Fortunewheel')}></Button> */}
-        </View>
+            <View>
+                <Text> Homepage {user.email}</Text>
+                <Button title='Logout'onPress={onSignoutPress} ></Button>
+            </View>
     )
 }
